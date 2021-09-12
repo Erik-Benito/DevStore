c088fa7b6567b1d9e52230c34c2ce736205df14d
Erik Benito <erikbenito_2017@outlook.com>
2021-09-12 20:33:20 -0300

    DevStore

--- api/src/db.js
+++ api/src/db.js
@@ -1,10 +1,10 @@
 import initdb from './models/init-models.js'
 import Sequelize from 'sequelize';
 const sequelize = new Sequelize(
-'devstore',
-'root',
-'Rtr171100,', {
-host: 'localhost',
+'mysql_17753_nsf2021',
+'nsf2021',
+'nsf@2021', {
+host: 'my01.winhost.com',
 dialect: 'mysql',
 logging: false
 });
